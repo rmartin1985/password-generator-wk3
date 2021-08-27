484c101c2faa95d8cc1f99d1258057f499cf3f65
Rick Martin <rmartin1985@att.net>
2021-08-27 17:49:13 -0500

    cleaned up the code some more

--- assets/script.js
+++ assets/script.js
@@ -27,36 +27,44 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    // prompt for character count
     var characters = prompt("How many characters would you like your password to contain?");
+    // make sure it is between 8 and 128 characters
     if ((parseInt(characters) < 8) || (parseInt(characters) > 128)) {
         alert("Please choose a number between 8 and 128");
         return writePassword();
     } 
+    // alert if they don't choose any characters 
     else if (!characters) {
         alert("You must select a number of characters for your password");
         return writePassword();
     } else {
         var length = parseInt(characters);
     }
+    // confirm if they want lower case letters
     var hasLower = confirm("Do you want to include lower case letters?");
+    // confirm if they want upper case letters
     var hasUpper = confirm("Do you want to include upper case letters?");
+    // confirm if they want numbers
     var hasNumber = confirm("Do you want to include numbers?");
+    // confirm if they want special characters 
     var hasSymbol = confirm("Do you want to include special characters?");
-  
-    var passwordEl = generatePassword(
+    // pass the answers into the generatePassword function
+    generatePassword(
         hasLower, 
         hasUpper, 
         hasNumber, 
         hasSymbol, 
         length
     );
-    console.log(passwordEl());
 };
      
-
+// create function to generate the password here 
 function generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length) {
+    // password needs to display as an array
     var password = ""
     var functionArray = []
+    // if they choose ok (true) for any of the prompts, add them to the array of possible characters
     if (hasLower) {
         functionArray.push(getRandomLower)
     }
@@ -69,10 +77,12 @@ function generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length) {
     if (hasSymbol) {
         functionArray.push(getRandomSymbol)
     }
+    // if they don't choose anything send them alert that they need to pick at least one and start over
     if (!hasLower && !hasUpper && !hasNumber && !hasSymbol) {
         alert("You must confirm at least one to continue");
         return writePassword();
     }
+    // for loop will go here to iterate through the length of the character count chosen and the random characters 
     for (var i = 0; i < length; i++ ) {
         var index = Math.floor(Math.random() * functionArray.length)
         password = password + functionArray[index]();
