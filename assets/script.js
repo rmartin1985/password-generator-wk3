e84e9e09174646d6b6c4323940944d6f5933f182
Rick Martin <rmartin1985@att.net>
2021-08-27 15:10:02 -0500

    created prompts and confirm windows

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+
+var randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
 // function to generate random lower case letters
 function getRandomLower () {
    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -35,10 +43,45 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var characters = prompt("How many characters would you like your password to contain?");
+    var length = parseInt(characters);
+    var hasLower = confirm("Do you want to include lower case letters?");
+    var hasUpper = confirm("Do you want to include upper case letters?");
+    var hasNumber = confirm("Do you want to include numbers?");
+    var hasSymbol = confirm("Do you want to include special characters?");
+  
+    console.log(length);
+    console.log(hasLower);
+    console.log(hasUpper);
+    console.log(hasNumber);
+    console.log(hasSymbol);
+  
+    var password = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbol, 
+        length
+    );
+};
+     
 
-  passwordText.value = password;
+// var passwordText = document.querySelector("#password");
+
+// passwordText.value = password; 
+
+
+function generatePassword(lower, upper, number, symbol, length) {
+
+    var generatedPassword = '';
+
+    var typesCount = lower + upper + number + symbol;
+
+    console.log('typesCount: ', typesCount);
+
+    var typesArr = [{lower}, {upper}, {number}, {symbol}];
+
+    console.log('typesArr: ', typesArr);
 
 }
 
