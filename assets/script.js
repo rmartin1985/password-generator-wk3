08715225d6b88f53e72627bb1b59694dd43ce6ed
Rick Martin <rmartin1985@att.net>
2021-08-27 13:13:12 -0500

    wrote functions to generate randoom numbers, letters, and symbols

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,34 @@
 // Assignment code here
 
+// function to generate random lower case letters
+function getRandomLower () {
+   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+// function to generate random upper case letters
+function getRandomUpper () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+ };
+
+// function to generate random numbers
+ function getRandomNumber () {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+ };
+
+// function to generate random symbols
+ function getRandomSymbol () {
+     var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+ };
+
+ console.log(getRandomLower());
+ console.log(getRandomUpper());
+ console.log(getRandomNumber());
+ console.log(getRandomSymbol());
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
